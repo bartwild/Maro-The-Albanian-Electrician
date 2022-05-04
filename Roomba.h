9ffe6e0427e76095cac5a1a8ab8c5a8cc3321eb2
zgodek <01168883@pw.edu.pl>
2022-05-04 23:05:27 +0200

    Roombas start moving when in sight.

--- Roomba.h
+++ Roomba.h
@@ -10,13 +10,12 @@ class Roomba{
 	float y;
 	float xSpeed;
 	float ySpeed;
-	unsigned char jumpTimer;
 	sf::Sprite sprite;
 	sf::Texture texture;
 public:
 	Roomba();
 	void draw(sf::RenderWindow& aWindow);
-	void move(const Map& aMap);
+	void move(const Map& aMap, const unsigned aViewX);
 	void set_position(float x, float y);
 	float get_x() const;
 	float get_y() const;
