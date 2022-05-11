bf3bc715dc01b2bbc2b2ad258b69a49d469b6de3
zgodek <01168883@pw.edu.pl>
2022-05-11 20:57:13 +0200

    Added get_hit_box and die.

--- Roomba.h
+++ Roomba.h
@@ -12,11 +12,16 @@ class Roomba{
 	float ySpeed;
 	sf::Sprite sprite;
 	sf::Texture texture;
+	bool dead;
+	unsigned char deathTimer;
+
 public:
 	Roomba();
 	void draw(sf::RenderWindow& aWindow);
-	void move(const Map& aMap, const unsigned aViewX);
+	void move(const Map& aMap, const unsigned aViewX, std::vector<Roomba>& aRoombas);
 	void set_position(float x, float y);
 	float get_x() const;
 	float get_y() const;
+	sf::FloatRect get_hit_box() const;
+	void die();
 };
