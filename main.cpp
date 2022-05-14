9d9a170c9cb027fced721282235e6c9a052f89fb
zgodek <01168883@pw.edu.pl>
2022-05-14 23:56:14 +0200

    Still cant fixed roomba erasing bug.

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ void whole_Game() {
 			viewX = std::clamp<int>(round(maro.get_x()) - 0.5f * (SCREEN_WIDTH - CELL_SIZE), 0, CELL_SIZE * map.size() - SCREEN_WIDTH);
 			maro.update(levelManager, viewX, map, roombas);
 			for (Roomba& roomba : roombas){
-				roomba.move(map, viewX, roombas);
+				roomba.update(map, viewX, roombas);
 			}
 			for (unsigned short i = 0; i < roombas.size(); i++){
 				if (roombas[i].get_death_timer() == 0){
