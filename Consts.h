fcbb7c60be4f299896d45b667d184bbd0d42a707
dbartos2 <01168871@pw.edu.pl>
2022-05-19 20:34:33 +0200

    Fixed Path so works on windows.

--- Consts.h
+++ Consts.h
@@ -34,8 +34,7 @@ const float ROOMBA_SPEED = 1;
 const float MUSHROOM_SPEED = 1;
 const float UPDATE_AREA = 64;
 const std::chrono::microseconds FRAME_DURATION(16667);
-const std::string CURRENT_PATH = std::filesystem::current_path();
-const std::string TEXTURES_PATH = CURRENT_PATH + "/textures/";
+const std::string TEXTURES_PATH = "textures/";
 const std::string MARO_WALK_TEXTURE = TEXTURES_PATH + "MaroWalk.png";
 
 enum Cell {
